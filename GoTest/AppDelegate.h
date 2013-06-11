e09b31b4f6f3925c038ca7e9e2c5cb357c7efeec
Hajime Hoshi <hajimehoshi@gmail.com>
2013-06-11 23:20:53 +0900

    Initial Commit

---
+++ GoTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoTest
+//
+//  Created by Hajime Hoshi on 6/11/13.
+//  Copyright (c) 2013 Hajime Hoshi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GoTest/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  GoTest
+//
+//  Created by Hajime Hoshi on 6/11/13.
+//  Copyright (c) 2013 Hajime Hoshi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
