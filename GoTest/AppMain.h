213f9c736215f36b8592ae7ab7aaf464acb60020
Hajime Hoshi <hajimehoshi@google.com>
2013-06-13 14:28:48 +0900

    Remove 'src' entry from the project

---
+++ GoTest/AppMain.h
@@ -0,0 +1,14 @@
+//
+//  AppMain.h
+//  GoTest
+//
+//  Created by Hajime Hoshi on 6/13/13.
+//  Copyright (c) 2013 Hajime Hoshi. All rights reserved.
+//
+
+#ifndef GoTest_AppMain_h
+#define GoTest_AppMain_h
+
+
+
+#endif
